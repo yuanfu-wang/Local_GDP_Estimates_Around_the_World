7c1ca39c244a2f4c09c22a5ee56ae279943da141
Yuanfu Wang <ywangmw@connect.ust.hk>
2026-08-04 15:47:24 -0500

    Update 11_certain_developing_isos_from_DOSE.R
    Use DOSE v2.14 and fill in the gap with older version (v2.11) for (a) 2020 observations and (b) missing population data

--- step2_obtain_gdp_data/11_certain_developing_isos_from_DOSE.R
+++ step2_obtain_gdp_data/11_certain_developing_isos_from_DOSE.R
@@ -4,6 +4,8 @@
 # -------------------------------------------------------------------------------- #
 
 # use R version 4.2.1 (2022-06-23) -- "Funny-Looking Kid"
+rm(list = ls())
+gc()
 
 Sys.getlocale()
 Sys.setlocale("LC_ALL", "en_US.UTF-8")
@@ -19,10 +21,54 @@ library(qgisprocess)
 iso_to_include <- c("THA", "MOZ", "UZB", "KEN", "VNM", "SRB", "ECU", "BLR",
                     "ALB", "LKA", "BIH")
 
-DOSE_gdp_pre <- read.csv("step2_obtain_gdp_data/inputs/gdp_data/regional/DOSE/DOSE_V2.11.csv")  %>% 
-  dplyr::select(c(GID_0, GID_1, year, grp_lcu, pop, grp_pc_lcu))  %>%  # grp_lcu means regional product in local currency
-  rename(iso = GID_0, id = GID_1)  %>% 
-  filter(year >= 2012, iso %in% iso_to_include)  %>% 
+# load DOSE v2.11 dataset
+DOSE_v2_11 <- read.csv("step2_obtain_gdp_data/inputs/gdp_data/regional/DOSE/DOSE_V2.11.csv") %>%
+  dplyr::select(c(GID_0, GID_1, year, grp_lcu, pop, grp_pc_lcu)) %>%  # grp_lcu means regional product in local currency
+  rename(iso = GID_0, id = GID_1) %>%
+  dplyr::filter(year >= 2012, iso %in% iso_to_include) %>%
+  arrange(iso, year, id)
+
+# load DOSE v2.14 dataset
+DOSE_v2_14 <- read.csv("step2_obtain_gdp_data/inputs/gdp_data/regional/DOSE/DOSE_V2.14.csv") %>%
+  dplyr::select(c(GID_0, GID_1, year, grp_lcu, pop, grp_pc_lcu)) %>%
+  rename(iso = GID_0, id = GID_1) %>%
+  dplyr::filter(year >= 2012, iso %in% iso_to_include) %>%
+  arrange(iso, year, id)
+
+# identify the keys used for merging
+merge_keys <- c("iso", "id", "year")
+
+# identify the region-year that has (1) the same GRP and (2) missing population in v2.14
+population_fallback_keys <- DOSE_v2_11 %>%
+  dplyr::select(all_of(merge_keys), grp_lcu_v2_11 = grp_lcu, pop_v2_11 = pop) %>%
+  inner_join(
+    DOSE_v2_14 %>%
+      dplyr::select(all_of(merge_keys), grp_lcu_v2_14 = grp_lcu, pop_v2_14 = pop), by = merge_keys
+  ) %>%
+  dplyr::filter(
+    !is.na(grp_lcu_v2_11),
+    !is.na(grp_lcu_v2_14),
+    grp_lcu_v2_11 == grp_lcu_v2_14,
+    !is.na(pop_v2_11),
+    is.na(pop_v2_14)
+  ) %>%
+  dplyr::select(all_of(merge_keys))
+
+# extract 2020 observations from v2.11
+DOSE_v2_11_priority <- bind_rows(
+    DOSE_v2_11 %>%
+      dplyr::filter(year == 2020),
+    DOSE_v2_11 %>%
+      semi_join(population_fallback_keys, by = merge_keys)
+  ) %>%
+  distinct(iso, id, year, .keep_all = TRUE)
+
+# add 2020 observations to v2.14; replace missing populations using v2.11 if GRP is the same
+DOSE_gdp_pre <- bind_rows(
+    DOSE_v2_14 %>%
+      anti_join(DOSE_v2_11_priority %>% dplyr::select(all_of(merge_keys)), by = merge_keys),
+    DOSE_v2_11_priority
+  ) %>%
   arrange(iso, year, id)
 
 # there are some years for some countries have missing gdp data
